ca8d57af45ef7f9faf8bd65220279b2d9f34ede6
Prithvi <prithvisinghtewatia@gmail.com>
2022-07-08 19:59:36 +0530

    initialized new project repo

---
+++ src/main/java/com/claimsmanagement/claimmanagement/ClaimManagementNewApplication.java
@@ -0,0 +1,13 @@
+package com.claimsmanagement.claimmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClaimManagementNewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClaimManagementNewApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/claimsmanagement/claimmanagement/ClaimManagementNewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.claimsmanagement.claimmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClaimManagementNewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
